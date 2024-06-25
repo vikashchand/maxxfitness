6a5dfc492a220969f3284fe6dd5a49e442ee8174
vikash chand <vikash.chand2020@vitstudent.ac.in>
2024-06-25 17:25:02 +0530

    news

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
@@ -8,7 +9,14 @@ import Programs from './Components/Programs/Programs';
 import Testimonial from './Components/Testimonial/Testimonial';
 import Why from './Components/Why/Why';
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 function App() {
+
+  useEffect(() => {
+    AOS.init({duration : 1000, once : true})
+  }, [])
+  
 
   return (
     <div className="App">
